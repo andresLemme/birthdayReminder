e177ad63b75f4d7bce5f80ab1352a6298f8c2951
Pocas1982 <andres.lemme@gmail.com>
2021-04-26 08:20:06 -0300

    create state and array map

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import List from './component/List';
+import { useState } from 'react';
+import data from './component/Data/data';
 
 function App() {
+  const [people, setPeople] = useState(data)
+
+  function handleClear(){
+    console.log("click")
+    setPeople([])
+  }
+  function handleAll(){
+    setPeople(data)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+    <section>
+     <h3>{people.length} birthday today </h3>
+     <List people={people}/>
+      <button onClick={handleClear}>Borrar lista</button>
+      <button onClick={handleAll}>Agregar lista</button>
+
+    </section>
+    </main>
   );
 }
 
--- src/component/List/index.js
+++ src/component/List/index.js
@@ -1,9 +1,21 @@
 import React from 'react'
 
-export default function List() {
+export default function List({people}) {
   return (
-    <div>
-      
-    </div>
+    <>
+      {people.map((person, key) =>{
+        return(
+          <article key={key}>
+            <img src={person.image} alt={person.name}/>
+            <div>
+              <h4>{person.name}</h4>
+              <p>{person.age} years</p>
+            </div>
+
+          </article>
+        )
+      })}
+
+    </>
   )
 }
